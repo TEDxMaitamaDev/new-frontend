6725b4f01fcfd1f1ead6d6993b5361c02a8ea55a
Mr. Paul Ishaili <paulishaili@gmail.com>
2025-08-15 17:08:57 +0100

    Significant updates on type, components and homepage

---
+++ src/components/ui/cards/EventPreview.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import React from "react";
+
+type EventPreview = { event: EventType };
+
+export default function EventPreviewCard({ event }: EventPreview) {
+  return (
+    <div className=" w-full md:w-64 flex flex-col gap-4 text-tedx-black">
+      <Image
+        className="h-40 w-full rounded-xl"
+        alt="Event preview"
+        src={event.image}
+        width={1024}
+        height={1024}
+        loading="lazy"
+      />
+      <div className="">
+        <h2 className="font-bold">{event.title}</h2>
+        <p className="text-sm">{event.description}</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/data/speakers.ts
@@ -0,0 +1,24 @@
+const speakers: Speaker[] = [
+  {
+    name: "Aisha Bello",
+    anchored_topic: "Innovate for a sustainable future.",
+    image: "",
+  },
+  {
+    name: "Aisha Bello",
+    anchored_topic: "Innovate for a sustainable future.",
+    image: "",
+  },
+  {
+    name: "Aisha Bello",
+    anchored_topic: "Innovate for a sustainable future.",
+    image: "",
+  },
+  {
+    name: "Aisha Bello",
+    anchored_topic: "Innovate for a sustainable future.",
+    image: "",
+  },
+];
+
+export default speakers;
